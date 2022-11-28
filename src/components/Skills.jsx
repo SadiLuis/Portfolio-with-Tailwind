e65daca1a16ebe2f84740655d2e319e75aa2251f
SadiLuis <84958501+SadiLuis@users.noreply.github.com>
2022-11-28 19:24:04 -0300

    Try to fixed bug in Tailwind

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -51,10 +51,7 @@ const Skills = () => {
                 <img className='w-20 mx-auto' src={ChakraUi} alt="HTML icon" />
                 <p className='my-4'>CHAKRA-UI</p>
             </div>
-            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-              <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
-              <p className='my-4'>GITHUB</p>
-            </div>
+           
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                 <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
                 <p className='my-4'>FIREBASE</p>
