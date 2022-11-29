1438d9cd2d7238c6e39f7a45406da9d9a4e5c4a9
SadiLuis <84958501+SadiLuis@users.noreply.github.com>
2022-11-28 22:31:12 -0300

    Sigo solucionando bug de los estilos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       {/* Container */}
-      <div className='max-w-[1000px] mx-aut px-10 flex flex-col justify-center h-full'>
+      <div className='max-w-[1000px] mx-auto px-10  flex flex-col justify-center h-full'>
         <p className='text-pink-600'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
           Sadi Rueda
