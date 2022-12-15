8a826e0cda2eeac4539715aa05864f862b2b4139
SadiLuis <84958501+SadiLuis@users.noreply.github.com>
2022-12-15 13:37:26 -0300

    Update CV

--- src/data/data.js
+++ src/data/data.js
@@ -1,49 +1,57 @@
 import Ecommerce from '../assets/projects/e-commerce.png';
-import Task from '../assets/projects/Task.png'
-import NetflixClone from '../assets/projects/netflix.png'
-import Chat from '../assets/projects/chat.png'
-import Login from '../assets/projects/login.png'
-import Dog from '../assets/projects/dog.png'
+import Task from '../assets/projects/Task.png';
+import NetflixClone from '../assets/projects/netflix.png';
+import Chat from '../assets/projects/chat.png';
+import Login from '../assets/projects/login.png';
+import Dog from '../assets/projects/dog.png';
+import SociaMedia from '../assets/projects/mediaApp.png'
 
 
 export const data=[
     {
         id:1,
+        name:"Social-Media",
+        image:SociaMedia,
+        github:"https://github.com/SadiLuis/Social-Media-App",
+        live:"https://social-media-appreact.web.app/",
+    },
+    {
+        id:2,
         name:"Netflix Clone",
         image:NetflixClone,
         github:"https://github.com/SadiLuis/React-Netflix-Clon",
         live:"https://netflix-clone-react-2ab91.web.app/",
     },
     {
-        id:2,
+        id:3,
         name:"Real time Chat",
         image:Chat,
         github:"https://github.com/SadiLuis/Chat-firebaseWithReact",
         live:"https://chat-react-76b11.firebaseapp.com/",
     },
     {
-        id:3,
+        id:4,
         name:"ReactJS Login",
         image:Login,
         github:"https://github.com/SadiLuis/Login-React-Firebase-ChakraUI",
         live:"https://login-react-9b927.web.app/",
     },
     {
-        id:4,
+        id:5,
         name:"Toodo app",
         image:Task,
         github:"https://github.com/SadiLuis/toodolist",
         live:"https://takslistapp.netlify.app/",
     },
     {
-        id:5,
+        id:6,
         name:"E-commerce Full-Stack",
         image:Ecommerce,
         github:"https://github.com/SadiLuis/E-commerce-React",
         live:"https://mobi-app.netlify.app/",
     },
     {
-        id:6,
+        id:7,
         name:"Dog api",
         image:Dog,
         github:"https://github.com/SadiLuis/ProyectoN-1Dogs",
