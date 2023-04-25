2b718775e3aad8bb162619e92cff0fa80989f4e0
SadiLuis <84958501+SadiLuis@users.noreply.github.com>
2023-04-25 10:07:50 -0300

    Adding new proyect

--- src/data/data.js
+++ src/data/data.js
@@ -8,74 +8,82 @@ import SociaMedia from '../assets/projects/mediaApp.png'
 import StoreNike from '../assets/projects/store-clone-nike.png';
 import StoreGaming from '../assets/projects/store.png';
 import Hotel from '../assets/projects/hotel.png';
+import Befree from '../assets/projects/Befree.png';
 
 
 export const data=[
     {
         id:1,
+        name:"BeFree",
+        image:Befree,
+       
+        live:"https://befree.one/",
+    },
+    {
+        id:2,
         name:"Hotel",
         image:Hotel,
         github:"https://github.com/SadiLuis/Tu-Hotel",
         live:"https://tu-hotel.netlify.app/",
     },
     {
-        id:2,
+        id:3,
         name: 'Nike Store-clone',
         image: StoreNike,
         github: 'https://github.com/SadiLuis/Nike-Clone-Store',
         live: 'https://nike-clone-store.vercel.app/',
     },
     {
-        id:3,
+        id:4,
         name: 'Gaming-Store',
         image: StoreGaming,
         github: 'https://github.com/SadiLuis/storeGaming',
         live: 'https://store-gaming-seven.vercel.app/'
     },
     {
-        id:4,
+        id:5,
         name:"Social-Media",
         image:SociaMedia,
         github:"https://github.com/SadiLuis/Social-Media-App",
         live:"https://social-media-appreact.web.app/",
     },
     {
-        id:5,
+        id:6,
         name:"Netflix Clone",
         image:NetflixClone,
         github:"https://github.com/SadiLuis/React-Netflix-Clon",
         live:"https://netflix-clone-react-2ab91.web.app/",
     },
     {
-        id:6,
+        id:7,
         name:"Real time Chat",
         image:Chat,
         github:"https://github.com/SadiLuis/Chat-firebaseWithReact",
         live:"https://chat-react-76b11.firebaseapp.com/",
     },
     {
-        id:7,
+        id:8,
         name:"ReactJS Login",
         image:Login,
         github:"https://github.com/SadiLuis/Login-React-Firebase-ChakraUI",
         live:"https://login-react-9b927.web.app/",
     },
     {
-        id:8,
+        id:9,
         name:"Toodo app",
         image:Task,
         github:"https://github.com/SadiLuis/toodolist",
         live:"https://takslistapp.netlify.app/",
     },
     {
-        id:9,
+        id:10,
         name:"E-commerce Full-Stack",
         image:Ecommerce,
         github:"https://github.com/SadiLuis/E-commerce-React",
         live:"https://mobi-app.netlify.app/",
     },
     {
-        id:10,
+        id:11,
         name:"Dog api",
         image:Dog,
         github:"https://github.com/SadiLuis/ProyectoN-1Dogs",
